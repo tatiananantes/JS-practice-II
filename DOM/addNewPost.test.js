497d152161064d5fd4d35b077d93753a11ac7d2b
Tatiana Nantes <tatiananantes@gmail.com>
2021-11-30 15:30:05 +0000

    adding an input - passing test

--- DOM/addNewPost.test.js
+++ DOM/addNewPost.test.js
@@ -8,12 +8,14 @@
 });
 
 test('creates and displays a new post', () => {
-  document.body.innerHTML = ` <button id="add-post-btn">Add post</button>
+  document.body.innerHTML = ` <input type="text" id="add-post">
+  <button id="add-post-btn">Add post</button>
   <div class="post" id="post-1"> A first post </div>
   <div class="post" id="post-2"> A second post </div>
   <div class="post" id="post-3"> A third post </div>`;
   
 require('./index');
+require('./addNewPost');
 
 // simulate a click on a button
   const button = document.querySelector('#add-post-btn');
--- DOM/index.js
+++ DOM/index.js
@@ -1,28 +1,12 @@
-// const addNewPost = require('./addNewPost');
+const addNewPost = require('./addNewPost');
 
-const button = document.querySelector('#add-post-btn');
-const post = document.querySelectorAll('.post');
+const buttonEl = document.querySelector('#add-post-btn');
+const inputEl = document.querySelector('input');
 
-button.addEventListener('click', () => {
-  addNewPost('Posty Post');
+buttonEl.addEventListener('click', () => {
+  const newTitle = inputEl.value;
+  addNewPost(newTitle);
 });
-
-const addNewPost = (title) => {
-  const newPostEl = document.createElement('div');
-  const newId = document.querySelectorAll('.post').length + 1;
-  
-  newPostEl.innerText = title;
-  newPostEl.className = 'post';
-  newPostEl.id = 'post-' + newId;
-  document.body.appendChild(newPostEl);
-}
-
-
-
-
-
-
-
 
 // const element = document.querySelector('#post-1');
 // element.innerText = 'A first post refreshed by JS';
